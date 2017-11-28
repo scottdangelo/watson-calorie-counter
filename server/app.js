1207e5aa30c128a0d61e0752530ee920efda29e2
Scott DAngelo <scott.dangelo@ibm.com>
2017-11-28 15:00:50 -0700

    Add code to test Promises.

--- server/app.js
+++ server/app.js
@@ -27,10 +27,95 @@ const vcapServices = require("vcap_services");
 const credentials = vcapServices.getCredentials("watson_vision_combined");
 
 const visual_recognition = watson.visual_recognition({
-    api_key: credentials.api_key, 
+    api_key: "f2e7cae6557d6386aaa13465545062319f84fa0d",
     version: "v3",
     version_date: "2016-05-20"
 });
+
+var custom_classifier = null;
+
+/**
+* promises
+*
+* get classifiers. Set global
+* create classifier. IF global set, return it
+* test classifier. Use global, or get first and then use
+* 
+* create funtion to chain them all...
+*/
+
+function listClassifiersPromise(){
+  var class_id;
+  return new Promise(function(resolve, reject){
+    visual_recognition.listClassifiers({},
+      function(err, response) {
+        if(err)
+          reject(err);
+        else
+          class_id = JSON.parse(JSON.stringify(result)).classifiers[0]["classifier_id"]; 
+          console.log("class_id: " + class_id);
+          if (~class_id.indexOf('dogs')){
+          custom_classifier = class_id
+          console.log("custom_classifier: " + custom_classifier);
+          }
+          resolve(JSON.parse(JSON.stringify(response)));
+      })
+  })
+}
+  
+function CreateClassifierPromise(){
+  return new Promise(function(resolve, reject){
+    if (custom_classifier != null){
+      resolve;
+    }
+    else
+    var params = {
+      name: 'vehicle-damage-analyzer',
+  BrokenWindshield_positive_examples: fs.createReadStream('./data/BrokenWindshield.zip'),
+    FlatTire_positive_examples: fs.createReadStream('./data/FlatTire.zip'),
+    MotorcycleAccident_positive_examples: fs.createReadStream('./data/MotorcycleAccident.zip'),
+    Vandalism_positive_examples: fs.createReadStream('./data/Vandalism.zip'),
+  negative_examples: fs.createReadStream('./data/Negatives.zip')
+};
+
+  visual_recognition.createClassifier(params,
+    function(err, response) {
+      if (err)
+        console.log(err);
+      else
+        console.log(JSON.stringify(response, null, 2));
+    })
+  })
+};
+
+function init_class(){
+  listClassifiersPromise()
+  .then(CreateClassifierPromise);
+}
+
+function init_classifier(){
+  var checkExistingClassifier = listClassifiersPromise();
+  var class_id;
+
+  checkExistingClassifier.then(function(result){
+    class_id = JSON.parse(JSON.stringify(result)).classifiers[0]["classifier_id"]; 
+    console.log("class_id: " + class_id);
+    if (~class_id.indexOf('dogs')){
+     custom_classifier = class_id
+     console.log("custom_classifier: " + custom_classifier);
+     }
+  })
+  .catch(function(error){
+    console.log("not found. Create one");
+  });
+   
+};
+  
+init_class();
+console.log("before: custom_class: " + custom_classifier);
+//init_classifier()
+console.log("after: custom_class: " + custom_classifier);
+
 application.use(express.static(__dirname + "/public"));
 application.post("/uploadpic", function (req, result) {
     const form = new formidable.IncomingForm();
@@ -39,20 +124,17 @@ application.post("/uploadpic", function (req, result) {
         if (err) {
             console.log(err);
         } else {
-            console.log(fields);
             const filePath = JSON.parse(JSON.stringify(files));
             const params = {
                 image_file: fs.createReadStream(filePath.myPhoto.path),
-                classifier_ids: ["food"]
+                classifier_ids: [custom_classifier],
+                threshold: 0
             };
-            console.log("params: " + JSON.parse(JSON.stringify(params.classifier_ids)));
             visual_recognition.classify(params, function (err, res) {
                 if (err) {
                     console.log(err);
                 } else {
-                    console.log("response: " + res);
                     const labelsvr = JSON.parse(JSON.stringify(res)).images[0].classifiers[0];
-                    console.log("labelsvr: " + labelsvr);
                     result.send({data: labelsvr});
                 }
             });
